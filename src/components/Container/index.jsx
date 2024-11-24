d957c7272664dc78702938e2384e8a6b7100c2a9
Pietro Ramos <pietroramos5@gmail.com>
2024-11-24 13:00:02 -0300

    ajustes gerais

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,10 @@
+import { React } from "react";
+import styles from "./Container.module.css";
+
+const Container = ({ children }) => {
+    return (
+        <section className={styles.container}>
+            {children}
+        </section>
+    );
+}
